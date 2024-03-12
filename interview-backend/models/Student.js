804e2a5c4b6688e2cc01aedeb7a4247737f215e7
AliElamir <ali.elamiir.a@gmail.com>
2024-03-12 11:24:00 +0300

    models created

---
+++ interview-backend/models/Student.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const studentSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    studentId: { type: Number, required: true },
+    courses: [{ type: Schema.Types.ObjectId, ref: "Course" }],
+  },
+  {
+    timestamps: true,
+  }
+)
+module.exports = mongoose.model("Student", studentSchema)
